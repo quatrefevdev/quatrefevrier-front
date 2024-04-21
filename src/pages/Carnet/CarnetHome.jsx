061c2d23fb7486d45cecb229f4783f12060d587e
Martin Rouy <martinrouy03@gmail.com>
2024-04-21 18:30:20 +0200

    add Footer and Header to Carnet/suivi + logo.svg

--- src/pages/Carnet/CarnetHome.jsx
+++ src/pages/Carnet/CarnetHome.jsx
@@ -1,28 +1,32 @@
 import "./carnetHome.scss";
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
 const CarnetHome = ({ id }) => {
   return (
-    <main
-      className="container startCarnet"
-      style={{ justifyContent: "space-around" }}
-    >
-      <h2>Bienvenue sur votre espace de suivi médical</h2>
-      <div className="carnet-buttons">
-        <Link to={"/MyAppointments/" + id}>
-          <button>Mes rendez-vous médicaux</button>
-        </Link>
-        <Link>
-          <button>Mes effets secondaires</button>
-        </Link>
-        <Link>
-          <button>Mon poids</button>
-        </Link>
-        <Link>
-          <button>Mes notes</button>
-        </Link>
-      </div>
-    </main>
+    <>
+      <main
+        className="container startCarnet"
+        style={{ justifyContent: "space-around" }}
+      >
+        <h2>Bienvenue sur votre espace de suivi médical</h2>
+        <div className="carnet-buttons">
+          <Link to={"/MyAppointments/" + id}>
+            <button>Mes rendez-vous médicaux</button>
+          </Link>
+          <Link>
+            <button>Mes effets secondaires</button>
+          </Link>
+          <Link>
+            <button>Mon poids</button>
+          </Link>
+          <Link>
+            <button>Mes notes</button>
+          </Link>
+        </div>
+      </main>
+      <Footer selected="suivi" />
+    </>
   );
 };
 
